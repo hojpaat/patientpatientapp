2e0be19a220ba0e185bc6859317524d7bfbec61b
Judit Horvath <judit.of.horvath@gmail.com>
2019-05-23 14:34:27 +0200

    create repo

---
+++ src/main/java/com/greenfoxacademy/patientpatientapp/PatientpatientappApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.patientpatientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientpatientappApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(PatientpatientappApplication.class, args);
+  }
+  
+}
