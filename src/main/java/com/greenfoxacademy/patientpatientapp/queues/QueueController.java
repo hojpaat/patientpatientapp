f39598ae2bb47f875e256b02ad34b2a1a4b01f7a
Unknown <judit.of.horvath@gmail.com>
2019-05-23 18:44:57 +0200

    feat(QueueController). add Delete/myqueue method

--- src/main/java/com/greenfoxacademy/patientpatientapp/queues/QueueController.java
+++ src/main/java/com/greenfoxacademy/patientpatientapp/queues/QueueController.java
@@ -1,17 +1,26 @@
 package com.greenfoxacademy.patientpatientapp.queues;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class QueueController {
 
-  private QueueServiceImpl queueServiceImpl;
+  private QueueService queueService;
 
   @Autowired
-  public QueueController(QueueServiceImpl queueServiceImpl) {
-    this.queueServiceImpl = queueServiceImpl;
+  public QueueController(QueueService queueService) {
+    this.queueService = queueService;
   }
 
+  @DeleteMapping("/myqueue")
+  public ResponseEntity deleteQueue(@RequestBody Queue queue){
+    queueService.deleteQueue(queue.getId());
+    return ResponseEntity.status(200).body("Changes done");
+  }
 }
